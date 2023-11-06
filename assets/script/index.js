9447e6f267eb00a8ee3dc386ba821b1a88eaed66
MC Guenette <mc.c.guenette@gmail.com>
2023-11-06 13:34:37 -0600

    Basic files added

---
+++ assets/script/index.js
@@ -0,0 +1,17 @@
+'use-strict';
+
+// On load animation
+function target(selector, parent = document) {
+    return parent.querySelector(selector);
+};
+
+function targetAll(selector, parent = document) {
+    return [...parent.querySelectorAll(selector)];
+};
+
+// the other way we did yesterday it spit out a console error so googled a new way to handle it
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+};
+  
+
