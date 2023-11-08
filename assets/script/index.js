5eaf5a632b277d28f81e03f2ef6da9a7ea9e3e00
MC Guenette <mc.c.guenette@gmail.com>
2023-11-07 22:12:13 -0600

    added disabled btn, changed styles and added validation

--- assets/script/index.js
+++ assets/script/index.js
@@ -14,54 +14,73 @@ function onEvent(event, selector, callback) {
     return selector.addEventListener(event, callback);
 };
   
-let count = 0;
-function numberOfGuesses() {
-    
-};
-
 // Generate a random number between 1 and 50
-const secretNumber = Math.floor(Math.random() * 50) + 1;
+let secretNumber = generateRandomNumber();
 let remainingGuesses = 5;
 
-// Get references to HTML elements
-const enterGuess = target('#input-number');
-const output = target('#message');
-const totalGuesses = target('#guesses');
-const btnGuess = target('#guess-button');
-const playAgain = target('#play-again');
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 50) + 1;
+}
 
-// Add an event listener to the 'Guess' button
-onEvent('click', btnGuess, function () {
+// Function to simplify getting elements by ID
+function target(id) {
+    return document.getElementById(id);
+}
+
+// Get references to HTML elements using the "target" function
+const enterGuess = target('input-number');
+const output = target('message');
+const totalGuesses = target('guesses');
+const btnGuess = target('guess-button');
+const playAgain = target('play-again');
+
+// Add an event listener to the "Guess" button
+onEvent('click', btnGuess, handleGuess);
+
+// Add an event listener to the input field to handle "Enter" key press
+onEvent('keydown', enterGuess, function (event) {
+    if (event.key === 'Enter') {
+        handleGuess();
+    }
+});
+
+function handleGuess() {
     if (remainingGuesses > 0) {
         const guess = parseInt(enterGuess.value);
 
-        if (!isNaN(guess)) {
+        if (!isNaN(guess) && guess >= 1 && guess <= 50) {
             if (guess === secretNumber) {
                 output.textContent = 'Congratulations! You guessed the correct number!';
                 btnGuess.disabled = true;
             } else if (guess < secretNumber) {
-                output.textContent = 'Try a higher number.';
+                output.textContent = 'My number is higher';
             } else {
-                output.textContent = 'Try a lower number.';
+                output.textContent = 'My number is lower';
             }
 
             remainingGuesses--;
             totalGuesses.textContent = remainingGuesses;
 
             if (remainingGuesses === 0) {
-                output.textContent = `Out of guesses! The secret number was ${secretNumber}.`;
+                output.textContent = `Out of guesses! My number was ${secretNumber}.`;
                 btnGuess.disabled = true;
             }
 
             enterGuess.value = '';
+        } else {
+            output.textContent = 'Please enter a valid number between 1 and 50.';
         }
     }
-});
+        // Disable the "Guess" button when there are no more attempts
+        if (remainingGuesses === 0) {
+            btnGuess.disabled = true;
+        }
+};
 
-// Add an event listener to the 'Play Again' button
+// Add an event listener to the "Play Again" button
 onEvent('click', playAgain, function () {
     // Reset the game
-    secretNumber = Math.floor(Math.random() * 50) + 1;
+    secretNumber = generateRandomNumber();
     remainingGuesses = 5;
     totalGuesses.textContent = remainingGuesses;
     output.textContent = '';
